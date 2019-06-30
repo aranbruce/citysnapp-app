7496e1877c9555fb4e51cab23654576e4a81b751
Aran <aranbruce@gmail.com>
2019-06-30 22:22:59 +0100

    adjusted styles for navigation

--- src/App.js
+++ src/App.js
@@ -21,30 +21,10 @@ class App extends Component {
 		return (
 			<AuthProvider>
 				<Router>
-					<PrivateRoute
-						authenticated={this.state.authenticated}
-						exact
-						path="/"
-						component={Discover}
-					/>
-					<PrivateRoute
-						authenticated={this.state.authenticated}
-						exact
-						path="/plan"
-						component={Plan}
-					/>
-					<PrivateRoute
-						authenticated={this.state.authenticated}
-						exact
-						path="/friends"
-						component={Friends}
-					/>
-					<PrivateRoute
-						authenticated={this.state.authenticated}
-						exact
-						path="/profile"
-						component={Profile}
-					/>
+					<PrivateRoute exact path="/" component={Discover} />
+					<PrivateRoute exact path="/plan" component={Plan} />
+					<PrivateRoute exact path="/friends" component={Friends} />
+					<PrivateRoute exact path="/profile" component={Profile} />
 					<Route exact path="/login" component={Login} />
 					<Route exact path="/sign-up" component={SignUp} />
 				</Router>
