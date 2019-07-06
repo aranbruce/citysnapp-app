9050f714d8b1a842b8612a50db28bce866a39977
Aran <aranbruce@gmail.com>
2019-07-06 11:19:58 +0100

    refactored authentication to account for loading

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,64 +1,58 @@
-import React, { useCallback } from "react";
+import React, { Component } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import app from "../authentication/base";
 import Button from "../components/button/button";
-import AuthenticationWrapper from "../components/authenticationWrapper/authenticationWrapper";
 import styles from "../components/form/form.module.sass";
 
 import logo from "../images/citySnappLogo.svg";
+import AuthenticationWrapper from "../components/authenticationWrapper/authenticationWrapper";
 
-const SignUp = ({ history }) => {
-	const handleSignUp = useCallback(
-		async event => {
-			event.preventDefault();
-			const { email, password } = event.target.elements;
-			try {
-				await app
-					.auth()
-					.createUserWithEmailAndPassword(
-						email.value,
-						password.value
-					);
-				history.push("/");
-			} catch (error) {
-				alert(error);
-			}
-		},
-		[history]
-	);
-
-	return (
-		<AuthenticationWrapper>
-			<img src={logo} alt="CitySnapp Logo" className={"logoImage"} />
-
-			<h1>Create an Account</h1>
-			<form onSubmit={handleSignUp} className={styles.form}>
-				<input
-					name="email"
-					type="email"
-					placeholder="Email"
-					aria-label="Email"
-					className={styles.input}
-					required
-				/>
-				<input
-					name="password"
-					type="password"
-					placeholder="Password"
-					aria-label="Password"
-					className={styles.input}
-					minLength={6}
-					maxLength={100}
-					required
-				/>
-				<Button form type="submit">
-					Sign Up
-				</Button>
-			</form>
-			<Link to={"/login"}>Login</Link>
-		</AuthenticationWrapper>
-	);
-};
+class SignUp extends Component {
+	handleSignUp = async event => {
+		event.preventDefault();
+		const { email, password } = event.target.elements;
+		try {
+			await app
+				.auth()
+				.createUserWithEmailAndPassword(email.value, password.value);
+			this.props.history.push("/");
+		} catch (error) {
+			alert(error);
+		}
+	};
+	render() {
+		return (
+			<AuthenticationWrapper>
+				<img src={logo} alt="CitySnapp Logo" className={"logoImage"} />
+				<h1>Create an Account</h1>
+				<form onSubmit={this.handleSignUp} className={styles.form}>
+					<input
+						name="email"
+						type="email"
+						placeholder="Email"
+						aria-label="Email"
+						className={styles.input}
+						required
+					/>
+					<input
+						name="password"
+						type="password"
+						placeholder="Password"
+						aria-label="Password"
+						className={styles.input}
+						minLength={6}
+						maxLength={100}
+						required
+					/>
+					<Button form type="submit">
+						Sign Up
+					</Button>
+				</form>
+				<Link to={"/login"}>Login</Link>
+			</AuthenticationWrapper>
+		);
+	}
+}
 
 export default withRouter(SignUp);
