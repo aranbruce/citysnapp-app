ce04424543341a75e979fd3985ce29a3345addc0
Aran <aranbruce@gmail.com>
2019-07-07 12:54:45 +0100

    added screen size and orientation message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.sass";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Media from "react-media";
 import app from "./authentication/base.js";
 import SignUp from "./pages/signup";
 import Login from "./pages/login";
@@ -41,45 +42,57 @@ class App extends Component {
 			return null;
 		}
 		return (
-			<Router>
-				<Route
-					exact
-					path="/"
-					render={() =>
-						authenticated ? (
-							<Redirect to="/discover" />
-						) : (
-							<Redirect to="login" />
-						)
-					}
-				/>
-				<PrivateRoute
-					exact
-					path="/discover"
-					component={Discover}
-					authenticated={authenticated}
-				/>
-				<PrivateRoute
-					exact
-					path="/plan"
-					component={Plan}
-					authenticated={this.state.authenticated}
-				/>
-				<PrivateRoute
-					exact
-					path="/friends"
-					component={Friends}
-					authenticated={this.state.authenticated}
-				/>
-				<PrivateRoute
-					exact
-					path="/profile"
-					component={Profile}
-					authenticated={this.state.authenticated}
-				/>
-				<Route exact path="/login" component={Login} />
-				<Route exact path="/sign-up" component={SignUp} />
-			</Router>
+			<Media query="(max-width: 425px)">
+				{matches =>
+					matches ? (
+						<Router>
+							<Route
+								exact
+								path="/"
+								render={() =>
+									authenticated ? (
+										<Redirect to="/discover" />
+									) : (
+										<Redirect to="login" />
+									)
+								}
+							/>
+							<PrivateRoute
+								exact
+								path="/discover"
+								component={Discover}
+								authenticated={authenticated}
+							/>
+							<PrivateRoute
+								exact
+								path="/plan"
+								component={Plan}
+								authenticated={this.state.authenticated}
+							/>
+							<PrivateRoute
+								exact
+								path="/friends"
+								component={Friends}
+								authenticated={this.state.authenticated}
+							/>
+							<PrivateRoute
+								exact
+								path="/profile"
+								component={Profile}
+								authenticated={this.state.authenticated}
+							/>
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/sign-up" component={SignUp} />
+						</Router>
+					) : (
+						<div style={{ textAlign: "center", marginTop: "4rem" }}>
+							This app is only available on portrait on mobile.
+							Please view it portrait on mobile for the best
+							experience
+						</div>
+					)
+				}
+			</Media>
 		);
 	}
 }
